31b4cca634112198699e8a5178ffdb24383288fb
sang <lethanhsang14082001@gmail.com>
2024-07-16 07:15:58 +0700

    [feat][grpc][Update]

---
+++ Course-3-GrpcWithSpringWebFlux/grpc-playground/src/main/java/com/sanglt/sec03/Lec01Scalar.java
@@ -0,0 +1,14 @@
+package com.sanglt.sec03;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lec01Scalar {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec01Scalar.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
