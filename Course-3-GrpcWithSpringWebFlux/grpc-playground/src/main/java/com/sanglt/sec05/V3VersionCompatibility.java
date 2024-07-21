cb725cc61b0923a0de8a9a118073a67dae7893bb
sang <lethanhsang14082001@gmail.com>
2024-07-21 09:24:36 +0700

    [feat][grpc][Update]

--- Course-3-GrpcWithSpringWebFlux/grpc-playground/src/main/java/com/sanglt/sec05/V3VersionCompatibility.java
+++ Course-3-GrpcWithSpringWebFlux/grpc-playground/src/main/java/com/sanglt/sec05/V3VersionCompatibility.java
@@ -1,24 +1,28 @@
 package com.sanglt.sec05;
 
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.sanglt.models.sec05.v1.Television;
+import com.sanglt.models.sec05.v3.Television;
+import com.sanglt.models.sec05.v3.Type;
 import com.sanglt.sec05.parser.V1Parser;
 import com.sanglt.sec05.parser.V2Parser;
+import com.sanglt.sec05.parser.V3Parser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class V1VersionCompatibility {
+public class V3VersionCompatibility {
 
-    private static final Logger log = LoggerFactory.getLogger(V1VersionCompatibility.class);
+    private static final Logger log = LoggerFactory.getLogger(V3VersionCompatibility.class);
 
     public static void main(String[] args) throws InvalidProtocolBufferException {
         var tv = Television.newBuilder()
                 .setBrand("samsung")
-                .setYear(2019)
+                .setType(Type.HD)
                 .build();
 
         V1Parser.parse(tv.toByteArray());
         V2Parser.parse(tv.toByteArray());
+        V3Parser.parse(tv.toByteArray());
+
     }
 
 }
