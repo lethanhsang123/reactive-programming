8e8405da42d895b11576d5ac533ee155734e1a24
sang <lethanhsang14082001@gmail.com>
2024-07-23 06:53:30 +0700

    [feat][gRPC][Update]

---
+++ Course-3-GrpcWithSpringWebFlux/grpc-playground/src/test/java/com/sanglt/test/sec08/GuessResponseHandler.java
@@ -0,0 +1,62 @@
+package com.sanglt.test.sec08;
+
+import com.sanglt.models.sec08.GuessRequest;
+import com.sanglt.models.sec08.GuessResponse;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class GuessResponseHandler implements StreamObserver<GuessResponse> {
+
+    private static final Logger log = LoggerFactory.getLogger(GuessResponseHandler.class);
+    private final CountDownLatch latch = new CountDownLatch(1);
+    private StreamObserver<GuessRequest> requestStreamObserver;
+    private int lower;
+    private int upper;
+    private int middle;
+
+    @Override
+    public void onNext(GuessResponse guessResponse) {
+        log.info("Attempt: {}, Result: {}", guessResponse.getAttempt(), guessResponse.getResult());
+        switch (guessResponse.getResult()) {
+            case TOO_LOW -> this.send(this.middle, this.upper);
+            case TOO_HIGH -> this.send(this.lower, this.middle);
+        }
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        latch.countDown();
+    }
+
+    @Override
+    public void onCompleted() {
+        requestStreamObserver.onCompleted();
+    }
+
+    private void send(int low, int high) {
+        this.lower = low;
+        this.upper = high;
+        this.middle = low + (high - low) / 2;
+        log.info("Client guessed {}", this.middle);
+        this.requestStreamObserver.onNext(GuessRequest.newBuilder().setGuess(this.middle).build());
+    }
+
+    public void start() {
+        this.send(0, 100);
+    }
+
+    public void await() {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setRequestStreamObserver(StreamObserver<GuessRequest> requestStreamObserver) {
+        this.requestStreamObserver = requestStreamObserver;
+    }
+}
