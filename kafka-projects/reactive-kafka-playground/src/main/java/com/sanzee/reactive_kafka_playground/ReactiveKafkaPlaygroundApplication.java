15f3b8642e014ad0f5e7b29eebac53e7342cc704
sang <lethanhsang14082001@gmail.com>
2024-10-13 22:05:01 +0700

    [feat][kafka-reactive][Update]

---
+++ kafka-projects/reactive-kafka-playground/src/main/java/com/sanzee/reactive_kafka_playground/ReactiveKafkaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.sanzee.reactive_kafka_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveKafkaPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveKafkaPlaygroundApplication.class, args);
+	}
+
+}
---
+++ kafka-projects/reactive-kafka-playground/src/test/java/com/sanzee/reactive_kafka_playground/ReactiveKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanzee.reactive_kafka_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
