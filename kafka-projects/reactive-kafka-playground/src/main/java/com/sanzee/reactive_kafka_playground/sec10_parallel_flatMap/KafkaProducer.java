c225b8ddbcc25509a34af6592812758dd7841d4b
sang <lethanhsang14082001@gmail.com>
2024-10-27 20:23:00 +0700

    [feat][kafka][demo]

---
+++ kafka-projects/reactive-kafka-playground/src/main/java/com/sanzee/reactive_kafka_playground/sec10_parallel_flatMap/KafkaProducer.java
@@ -0,0 +1,44 @@
+package com.sanzee.reactive_kafka_playground.sec10_parallel_flatMap;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
+
+import java.util.Map;
+
+/*
+    goal: To demo partition re-balancing. Ensure that topic has multiple partitions.
+ */
+public class KafkaProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+
+    public static void main(String[] args) {
+        var producerConfig = Map.< String, Object >of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        );
+
+        var options = SenderOptions.<String, String>create(producerConfig);
+
+        var flux = Flux.range(1, 100)
+                .map(i -> new ProducerRecord<>("order-events", i.toString(), "order-" + i))
+                .map(pr -> SenderRecord.create(pr, pr.key()));
+
+        var sender = KafkaSender.create(options);
+        sender.send(flux)
+                .doOnNext(r -> log.info("Correlation id: {}", r.correlationMetadata()))
+                .doOnComplete(sender::close)
+                .subscribe();
+
+    }
+
+
+}
